fee22c028aebade3134577fd5003d4d561506e4b
Franciszek Gozdecki <s27379@pjwstk.edu.pl>
2024-04-22 18:47:08 +0200

    Added methods for selecting, inserting, updating and deleting the animals.

---
+++ apbd-cw6/apbd-cw6/Models/DTOs/AddAnimal.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apbd_cw6.Models.DTOs;
+
+public class AddAnimal
+{
+    [Required] [MaxLength(200)] public string Name { get; set; }
+    [MaxLength(200)] public string? Description { get; set; }
+    [Required] [MaxLength(200)] public string Category { get; set; }
+    [Required] [MaxLength(200)] public string Area { get; set; }
+}
